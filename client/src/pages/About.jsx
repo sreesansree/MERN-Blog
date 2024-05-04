6231f112221f005d072073b3b232b7e882458303
SREESANKAR-P <sreesankarputhumana@gmail.com>
2024-05-05 00:07:18 +0530

    created the header section

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+// import { Button } from "flowbite-react";
 export default function About() {
   return (
     <div>
       about
+      {/* <Button>Click me</Button> */}
     </div>
   );
 }
