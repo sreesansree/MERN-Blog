cfa79a38cbb547912a39828f9eb8bc67a9dc1da8
SREESANKAR-P <sreesankarputhumana@gmail.com>
2024-05-12 15:59:28 +0530

    add Google OAuth Functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema(
     },
     profilePicture: {
       type: String,
-      dafult: "",
+      dafult: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
     },
   },
   { timestamps: true }
