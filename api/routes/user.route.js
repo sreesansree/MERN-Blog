3100ec81cf117b844ebf2580a3d8413e46e5548b
SREESANKAR-P <sreesankarputhumana@gmail.com>
2024-06-09 17:05:20 +0530

    create get users api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   deleteUser,
+  getUsers,
   signout,
   test,
   updateUser,
@@ -14,5 +15,6 @@ router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
+router.get("/getusers", verifyToken, getUsers);
 
 export default router;
